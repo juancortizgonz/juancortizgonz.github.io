c70a6735d0da5e885e0284405f52aa119d7f820e
Juan Camilo Ortiz <juan.ortiz.gonzalez@correounivalle.edu.co>
2025-01-20 16:26:03 -0500

    Fix base in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://juancortizgonz.github.io',
-  base: 'my-portfolio',
+  base: '',
 });
